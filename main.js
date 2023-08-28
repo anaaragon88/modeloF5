9791708a4ed9bafa4fb0d9eca37ff3d73bff9f5c
Ana Aragon <ana.aragon88@gmail.com>
2023-08-28 14:08:58 +0200

    subir

---
+++ main.js
@@ -0,0 +1,31 @@
+import { loadGLTF } from "./libs/loader.js";
+
+const THREE = window.MINDAR.IMAGE.THREE;
+
+document.addEventListener('DOMContentLoaded', () => {
+  const start = async () => {
+
+    const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+      container: document.body,
+      imageTargetSrc: './assets/targetf5.mind',
+    });
+    const { renderer, scene, camera } = mindarThree;
+
+    const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+    scene.add(light);
+
+    const gltf = await loadGLTF('./assets/logof5.gltf');
+    gltf.scene.scale.set(10, 10, 10);
+    gltf.scene.position.set(0, -0.4, 0);
+
+    const anchor = mindarThree.addAnchor(0);
+    anchor.group.add(gltf.scene);
+
+    await mindarThree.start();
+    renderer.setAnimationLoop(() => {
+
+      renderer.render(scene, camera);
+    });
+  }
+  start();
+});
